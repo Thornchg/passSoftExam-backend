98563f940ac8909a2759411e24827557a4437e7a
隐辰 <thornchg@163.com>
2018-12-28 15:08:22 +0800

    创建项目

---
+++ src/main/java/com/thornchg/rkt/PassSoftExamApplication.java
@@ -0,0 +1,14 @@
+package com.thornchg.rkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassSoftExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassSoftExamApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/thornchg/rkt/PassSoftExamApplicationTests.java
@@ -0,0 +1,17 @@
+package com.thornchg.rkt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PassSoftExamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
